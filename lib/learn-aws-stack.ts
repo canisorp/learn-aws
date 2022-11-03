e12ea816f5079977841a80802c16525c6a1c2e23
Filip Basic <fbaso95@gmail.com>
2022-11-03 09:18:24 +0100

    add aws lambda

--- lib/learn-aws-stack.ts
+++ lib/learn-aws-stack.ts
@@ -1,9 +1,15 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class LearnAwsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class LearnAwsStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-  }
-}
+    // defines AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,   // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from lambda directory
+      handler: 'hello.handler'               // file is "hello", function is "handler"
+    });
+  };
+};
